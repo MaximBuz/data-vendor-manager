eb72d739718c086c543a7b80bac17e436bb85931
Maxim Buz <mbuz.maxim@gmail.com>
2021-12-08 18:12:24 +0100

    added dynamic depth serializer

--- frontend/frontend/frontend_vendormanager/pages/cost-manager/index.js
+++ frontend/frontend/frontend_vendormanager/pages/cost-manager/index.js
@@ -1,9 +1,3 @@
 export default function Home() {
-  const fetchData = async () => {
-    const url = new URL("http://localhost:8000/api/data_consumers/");
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => console.log(data));
-  };
-  return <h1 onClick={fetchData}>Cost Manager</h1>;
+  return <h1>Cost Manager</h1>;
 }
