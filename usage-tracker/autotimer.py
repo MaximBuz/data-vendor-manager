dd6e42d9981ddcacbb7dc372fca3c011f8a2ba2e
Maxim Buz <mbuz.maxim@gmail.com>
2021-12-13 18:10:07 +0100

    changed to using API

--- usage-tracker/autotimer.py
+++ usage-tracker/autotimer.py
@@ -4,41 +4,38 @@ from os import system
 from activity import *
 import datetime
 import sys
-if sys.platform in ['Windows', 'win32', 'cygwin']:
+
+if sys.platform in ["Windows", "win32", "cygwin"]:
     import win32gui
     import uiautomation as auto
-elif sys.platform in ['Mac', 'darwin', 'os2', 'os2emx']:
+elif sys.platform in ["Mac", "darwin", "os2", "os2emx"]:
     from AppKit import NSWorkspace
     from Foundation import *
-elif sys.platform in ['linux', 'linux2']:
-        import linux as l
+elif sys.platform in ["linux", "linux2"]:
+    import linux as l
 
-from crud import engine
-from models import UsageEntry
-from sqlalchemy.orm import sessionmaker
-Session = sessionmaker(bind=engine)
-s = Session()
+import requests
 
-from aggregated_view import fetch_aggregated_data
+user_id = input("Please type in your user_id: ")
+url = f"http://127.0.0.1:8000/api/usage/{user_id}"
 
 active_window_name = ""
 activity_name = ""
-start_time = datetime.datetime.now()
+start_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 first_time = True
 
-user = input("Please type in your email adress: ")
 
 def get_active_window():
     _active_window_name = None
-    if sys.platform in ['Windows', 'win32', 'cygwin']:
+    if sys.platform in ["Windows", "win32", "cygwin"]:
         window = win32gui.GetForegroundWindow()
         _active_window_name = win32gui.GetWindowText(window)
-    elif sys.platform in ['Mac', 'darwin', 'os2', 'os2emx']:
-        _active_window_name = (NSWorkspace.sharedWorkspace()
-                               .activeApplication()['NSApplicationName'])
+    elif sys.platform in ["Mac", "darwin", "os2", "os2emx"]:
+        _active_window_name = NSWorkspace.sharedWorkspace().activeApplication()[
+            "NSApplicationName"
+        ]
     else:
-        print("sys.platform={platform} is not supported."
-              .format(platform=sys.platform))
+        print("sys.platform={platform} is not supported.".format(platform=sys.platform))
         print(sys.version)
     return _active_window_name
 
@@ -65,37 +62,30 @@ def get_active_window():
 try:
     while True:
         previous_site = ""
-        if sys.platform not in ['linux', 'linux2']:
+        if sys.platform not in ["linux", "linux2"]:
             new_window_name = get_active_window()
-        if sys.platform in ['linux', 'linux2']:
+        if sys.platform in ["linux", "linux2"]:
             new_window_name = l.get_active_window_x()
-        
-        if active_window_name != new_window_name: 
+
+        if active_window_name != new_window_name:
             print(active_window_name)
             activity_name = active_window_name
 
-            if not first_time: 
-                end_time = datetime.datetime.now()
-                time_entry = TimeEntry(start_time, end_time)
-                print(time_entry.serialize())
-                # Hier in Datenbank schreiben!
-                if activity_name == "Google Chrome": # Hier nach BBG prüfen
-                    usage_entry = UsageEntry(
-                        user=user,
-                        app=activity_name,
-                        start_time=start_time,
-                        end_time=end_time
-                    )
-                    s.add(usage_entry)
-                start_time = datetime.datetime.now()
+            if not first_time:
+                end_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                if activity_name == "Google Chrome":  # Hier nach BBG prüfen
+                    # Hier POST request an Django Backend
+                    payload = {
+                        "app_name": activity_name,
+                        "start_time": start_time,
+                        "end_time": end_time,
+                        "data_consumer": user_id,
+                    }
+                    response = requests.post(url, data=payload)
+                start_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             first_time = False
             active_window_name = new_window_name
-
         time.sleep(1)
-    
+
 except KeyboardInterrupt:
-    s.commit()
-    s.close()
     print(" done")
-    print("Here is your data: ")
-    fetch_aggregated_data(user)
--- usage-tracker/crud.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine
-from config import DATABASE_URI
-
-engine = create_engine(DATABASE_URI)
-
-def recreate_database(Base):
-    Base.metadata.drop_all(engine)
-    Base.metadata.create_all(engine)
