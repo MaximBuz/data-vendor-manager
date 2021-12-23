f6e19f4c05a8e0ab6f770034c13b9e741679120e
Maxim Buz <mbuz.maxim@gmail.com>
2021-12-23 15:33:41 +0100

    created bloomberg license tree serializer

--- frontend/frontend/frontend_vendormanager/pages/master-data-manager/vendors/index.js
+++ frontend/frontend/frontend_vendormanager/pages/master-data-manager/vendors/index.js
@@ -1,7 +1,29 @@
-export default function vendors() {
-    return (
-        <>
-            
-        </>
-    )
+/* ------------------------------------------------------------------------- */
+/* ~~~~~~IMPORTS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+/* ------------------------------------------------------------------------- */
+
+/* ROUTING */
+import Link from "next/link";
+
+/* COMPONENTS */
+import TreeDataTable from "../../../components/tables/TreeDataTable";
+import { Button, Tooltip, Divider } from "antd";
+
+/* ------------------------------------------------------------------------- */
+/* ~~~~~~COMPONENT~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+/* ------------------------------------------------------------------------- */
+export default function Vendors() {
+  
+  /* --------------------------------------------------------------------------- */
+  /* ~~~~~~RENDERING~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+  /* --------------------------------------------------------------------------- */
+  return (
+    <>
+      <h2>Managing Vendors and Licenses</h2>
+      <p style={{ maxWidth: "550px" }}>
+        Here you can add new vendors and manage your licenses at those vendors.
+      </p>
+        <Divider orientation="left">Bloomberg</Divider>
+    </>
+  );
 }
