95e75b2ccf0cb4ec7f4f2be566df3a48e33af5f4
Maxim Buz <mbuz.maxim@gmail.com>
2022-01-11 11:03:27 +0100

    added api fetcher for aggregated usage

--- frontend/frontend/frontend_vendormanager/pages/cost-manager/index.js
+++ frontend/frontend/frontend_vendormanager/pages/cost-manager/index.js
@@ -1,3 +1,32 @@
+/* ------------------------------------------------------------------------- */
+/* ~~~~~~IMPORTS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+/* ------------------------------------------------------------------------- */
+
+/* API FETCHING */
+import {
+  dehydrate,
+  QueryClient,
+  useQuery,
+  useMutation,
+  useQueryClient,
+} from "react-query";
+import getAggregatedUsage from "../../api_utils/api_fetchers/getAggregatedUsage";
+
+
 export default function Home() {
+
+  /* -----~~~~~>>>DATAFETCHING<<<~~~~~----- */
+  const usageDataQuery = useQuery(
+    ["aggregatedUsage",
+    "data-consumer",
+    {
+      start_date: "2022-01-01"
+    }
+    ],
+    getAggregatedUsage
+  );
+  const usageData = usageDataQuery?.data;
+  console.log(usageData);
+
   return <h1>Cost Manager</h1>;
 }
