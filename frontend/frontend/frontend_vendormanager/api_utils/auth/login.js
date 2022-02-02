47cc30ed55c03bd4a8355d584289932a25b7cfe1
Maxim Buz <mbuz.maxim@gmail.com>
2022-02-02 12:15:16 +0100

    added custom error messages for unauthorized or lacking permission http errors

--- frontend/frontend/frontend_vendormanager/api_utils/auth/login.js
+++ frontend/frontend/frontend_vendormanager/api_utils/auth/login.js
@@ -3,11 +3,10 @@ import apiClient from "../http-common";
 export default async function login(formData) {
     
     // Get authentication token from backend
-    const { data, success } = await apiClient.post(`auth/login/`, formData)
+    const { data } = await apiClient.post(`auth/login/`, formData)
 
     // Save Token in local storage
     localStorage.setItem('authenticationToken', data.key);
-    console.log(localStorage);
 
     return data;
 }
--- frontend/frontend/frontend_vendormanager/api_utils/http-common.js
+++ frontend/frontend/frontend_vendormanager/api_utils/http-common.js
@@ -6,6 +6,9 @@ const httpCommon =  axios.create({
   baseURL: endpoint,
   headers: {
       "Content-Type": "application/json",
+  },
+  validateStatus: function (status) {
+    return status >= 200 && status < 300; // default
   }
 });
 
@@ -17,6 +20,23 @@ httpCommon.interceptors.request.use((config) => {
     config.headers.Authorization = `Token ${token}`;
   }
   return config;
+});
+
+
+// intercept every response to provide custom message on different status codes
+httpCommon.interceptors.response.use((response) => {
+  // Any status code that lie within the range of 2xx cause this function to trigger
+  // Do something with response data
+  return response;
+}, function (error) {
+  // Any status codes that falls outside the range of 2xx cause this function to trigger
+  // Do something with response error
+  if (error.response.status == 403) {
+    return Promise.reject("Insufficient permissions, please contact the responsible administrative staff to provide you with new permissions")
+  } else if (error.response.status == 401) {
+    return Promise.reject("Request could not be authenticated. Please login to use access this functionality")
+  }
+  return Promise.reject(error);
 });
 
 
