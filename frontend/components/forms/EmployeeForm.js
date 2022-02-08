fc87e025efb7f6524a345fd7a900a3f6649d7b08
Maxim Buz <mbuz.maxim@gmail.com>
2022-02-08 17:40:09 +0100

    blyat

--- frontend/components/forms/EmployeeForm.js
+++ frontend/components/forms/EmployeeForm.js
@@ -61,6 +61,10 @@ export default function EntityForm({
  /* -----~~~~~>>>DYNAMIC BUILDINGS DROPDOWN<<<~~~~~----- */
  const [activeLocation, setActiveLocation] = useState(initialValues?.location?.id || undefined);
 
+ console.log(locations);
+ console.log(locations?.filter((location) => location.id === activeLocation));
+ console.log(locations?.filter((location) => location.id === activeLocation)[0]);
+
  function handleLocationChange(value) {
   setActiveLocation(value);
   form.setFieldsValue({
@@ -184,14 +188,14 @@ export default function EntityForm({
 
      <Form.Item style={{ flexGrow: '1' }} name='location' label='Location'>
       <Select onChange={handleLocationChange}>
-       {locations &&
+       {/* {locations &&
         locations.map((location) => {
          return (
           <Select.Option value={location.id} key={location.id}>
            {location.street + ' ' + location.street_nr + ', ' + location.city + ', ' + location.country}
           </Select.Option>
          );
-        })}
+        })} */}
       </Select>
      </Form.Item>
 
