dc6f09ff507f17c88f4d9216975636bf878bfe23
Maxim Buz <mbuz.maxim@gmail.com>
2022-02-08 17:20:52 +0100

    added logging

--- frontend/components/forms/EmployeeForm.js
+++ frontend/components/forms/EmployeeForm.js
@@ -64,6 +64,7 @@ export default function EntityForm({
  console.log(locations);
  console.log(locations?.filter((location) => location.id === activeLocation));
  console.log(locations?.filter((location) => location.id === activeLocation)[0]);
+ console.log(locations?.filter((location) => location.id === activeLocation)[0].buildings);
 
  function handleLocationChange(value) {
   setActiveLocation(value);
